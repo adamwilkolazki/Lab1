8bfe6f3a7426c7b2ed9f3caa8b776cec27ce7564
adamwilkoilazki <adam.wilkolazki@gmail.com>
2022-03-16 19:52:59 +0100

    Punkt 8

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,13 +1,14 @@
 package pl.lublin.wsei.java.cwiczenia;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 import static java.lang.Math.sqrt;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("alfa\tsin(alfa)\n");
+     /*   System.out.printf("alfa\tsin(alfa)\n");
         for (int i = 0; i < 370; i += 10 )
             System.out.printf("%d\t%f\t\n",i, Math.sin(i/360.0*2*Math.PI));
 
@@ -16,11 +17,20 @@ public class Main {
         double sumaPoteg=0;
         double a=2;
 
-        for (int i = 1; i < 10; i += 1 ) {
+        for (int i = 0; i < 10; i += 1 ) {
             sumaPoteg+=Math.pow(a,i);
 
 
-            System.out.printf("%d\t%f\t%.0f\n",i,Math.log(i),sumaPoteg);
+            System.out.printf("%d\t%f\t%.0f\n",i,Math.log(i),sumaPoteg);*/
+        Scanner input = new Scanner(System.in);
+        int num1;
+        int num2;
+        System.out.println("Podaj pierwsza liczbe: ");
+        num1=input.nextInt();
+        System.out.println("Podaj druga liczbe: ");
+        num2=input.nextInt();
+        System.out.printf("wynik dodawania %d + %d = %d\n", num1, num2, num1 + num2 );
+
         }
 
 
@@ -29,4 +39,4 @@ public class Main {
 
 
     }
-}
+
