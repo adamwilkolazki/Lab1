7614e6171e01bbb94faacdbf7240a1a58c20d4a9
adamwilkoilazki <adam.wilkolazki@gmail.com>
2022-03-16 19:34:49 +0100

    Punkt 7

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,6 +2,8 @@ package pl.lublin.wsei.java.cwiczenia;
 
 import java.util.Arrays;
 
+import static java.lang.Math.sqrt;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,8 +13,20 @@ public class Main {
 
 
         System.out.printf("arg\tlog2(arg)\tsum(arg)\n");
-        for (int i = 1; i < 10; i += 1 )
-            System.out.printf("%d\t%f\t\n",i, Math.log(i));
+        double sumaPoteg=0;
+        double a=2;
+
+        for (int i = 1; i < 10; i += 1 ) {
+            sumaPoteg+=Math.pow(a,i);
+
+
+            System.out.printf("%d\t%f\t%.0f\n",i,Math.log(i),sumaPoteg);
+        }
+
+
+
+
+
 
     }
 }
